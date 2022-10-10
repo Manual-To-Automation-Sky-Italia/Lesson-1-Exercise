da362e8ede19706c83b1b52470e6c91891ef94ae
garyb-bs <gary.b@browserstack.com>
2022-10-10 14:39:37 +0100

    Initial commit

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,31 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.browserstack.com/users/sign_in");
+        driver.manage().window().maximize();
+
+        WebElement usernameElement = driver.findElement(By.cssSelector("#user_email_login"));
+        usernameElement.sendKeys(System.getenv("LOGIN_USERNAME"));
+
+        WebElement passwordElement = driver.findElement(By.cssSelector("#user_password"));
+        passwordElement.sendKeys(System.getenv(System.getenv("LOGIN_PASSWORD")));
+
+        WebElement submitButtonElement = driver.findElement(By.cssSelector("#user_submit"));
+        submitButtonElement.click();
+
+        Thread.sleep(5000);
+
+        WebElement bellIconElement = driver.findElement(By.id("beamer-bell"));
+        assert bellIconElement.isDisplayed();
+
+        driver.quit();
+    }
+}
